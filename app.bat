5e7634594c0ebd7c080159657becc648d935b7ee
Tristan Bettany <tristan.bettany@gmail.com>
2020-11-28 15:12:52 +0000

    Initial Commit

---
+++ app.bat
@@ -0,0 +1,70 @@
+@ECHO OFF
+
+for /f "delims== tokens=1,2" %%G in (.env) do set %%G=%%H
+
+CALL :CASE_%1
+IF ERRORLEVEL 1 CALL :DEFAULT_CASE
+
+ECHO Done!
+EXIT /B
+
+:CASE_rebuild
+    ECHO Stopping...
+    docker-compose down -v
+    ECHO Building...
+    docker-compose up -d --build
+    GOTO END_CASE
+
+:CASE_build
+    ECHO Building...
+    docker-compose up -d --build
+    GOTO END_CASE
+
+:CASE_restart
+    ECHO Stopping...
+    docker-compose down
+    ECHO Starting...
+    docker-compose up -d
+    GOTO END_CASE
+
+:CASE_stop
+    ECHO Stopping...
+    docker-compose down
+    GOTO END_CASE
+
+:CASE_start
+    ECHO Starting...
+    docker-compose up -d
+    GOTO END_CASE
+
+:CASE_install
+    ECHO Removing temporary application files...
+    @RD /S /Q "vendor"
+    ECHO Installing composer dependencies...
+    docker container exec -w %WORKING_DIR% %CLI_PHP_CONTAINER% composer install
+    GOTO END_CASE
+
+:CASE_update
+    ECHO Updating composer dependencies...
+    docker container exec -w %WORKING_DIR% %CLI_PHP_CONTAINER% composer update
+    GOTO END_CASE
+
+:CASE_dump
+    ECHO Dumping autoload...
+    docker container exec -w %WORKING_DIR% %CLI_PHP_CONTAINER% composer dump-autoload
+    GOTO END_CASE
+
+:CASE_key
+    ECHO Generating Key...
+    docker container exec -w %WORKING_DIR% %CLI_PHP_CONTAINER% php artisan key:generate
+    GOTO END_CASE
+
+:CASE_bash
+    docker container exec -it -w %WORKING_DIR% %CLI_PHP_CONTAINER% bash
+    GOTO END_CASE
+
+:DEFAULT_CASE
+    ECHO Unknown function %1
+    GOTO END_CASE
+:END_CASE
+    GOTO :EOF
